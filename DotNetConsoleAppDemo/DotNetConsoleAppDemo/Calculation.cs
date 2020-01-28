94a346302b0a162c695f5f0ef4ca1f8d90547513
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-01-28 15:25:29 +0200

    Lisätty C#-yksikkötestikoodia sekä Angular TypeScript-projekti.

---
+++ DotNetConsoleAppDemo/DotNetConsoleAppDemo/Calculation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetConsoleAppDemo
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Calculation
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ DotNetConsoleAppDemo/DotNetConsoleAppDemoTests/CalculationTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DotNetConsoleAppDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetConsoleAppDemo.Tests
+{
+    [TestClass()]
+    public class CalculationTests
+    {
+        [TestMethod()]
+        public void SumTest()
+        {
+            int a = 100;
+            int b = 200;
+
+            Calculation calc = new Calculation();
+            int sum = calc.Sum(a, b);
+
+            int expect = a + b;
+            Assert.AreEqual(expect, sum);
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            int a = 5;
+            int b = 10;
+
+            Calculation calc = new Calculation();
+            int result = calc.Multiply(a, b);
+
+            int expect = a * b;
+            Assert.AreEqual(expect, result);
+        }
+    }
+}
